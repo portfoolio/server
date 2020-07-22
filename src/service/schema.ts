df6c838ef153de526a0bea3f38a94aa03adc6b89
Djordje Stojiljkovic <djordjestojilljkovic@gmail.com>
2020-07-22 13:32:19 +0200

    Fixed few bugs.

--- src/service/schema.ts
+++ src/service/schema.ts
@@ -3,6 +3,7 @@ import mongooseSchema from 'src/core/util/mongooseSchema';
 export const serviceSchema = {
   title: String,
   description: String,
+  icon: String,
 };
 
 export const ServiceSchema = mongooseSchema(serviceSchema);
